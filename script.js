23735a7b716496037aed585d04f205bd924351a4
Natalia <nataliakam983@gmail.com>
2020-11-22 18:18:04 +0200

    Changed API and proxy

--- script.js
+++ script.js
@@ -1,54 +1,50 @@
 const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote');
+const twitterButton = document.getElementById('twitter-button');
+const newQuoteButton = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
-//Show Loading
+//show loading element
 function loading() {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-//Hide Loading
-function complete(){
+function complete() {
     if (!loader.hidden) {
-        quoteContainer.hidden = false;
         loader.hidden = true;
+        quoteContainer.hidden = false;
     }
 }
 
-// Get Quote From API
+// Get Quote from API
 async function getQuote() {
     loading();
-    //const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    const apiUrl = 'http://quotes.stormconsultancy.co.uk/random.json';
+    //get quote
+    const proxyUrl = 'https://sleepy-beach-46837.herokuapp.com/';
+    const apiUrl ='http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
-        const responce = await fetch (apiUrl);
-        const data = await responce.json();
-        // if Author is blank, add 'Unknown'
-        if (data.author === '') {
-            authorText.innerText = 'Unknown';
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        if (data.quoteAuthor ==='') {
+            authorText.innerText = "Unknown";
         } else {
-            authorText.innerText = data.author
+            authorText.innerText = data.quoteAuthor;
         }
-        //Reduce font size for long quotes
-        if (data.quote.length > 120) {
+        //Reduce font-size for long quotes
+        if (data.quoteText.length > 50){
             quoteText.classList.add('long-quote');
         } else {
-            quoteText.classList.remove('long-quote')
+            quoteText.classList.remove('long-quote');
         }
-        quoteText.innerText = data.quote
-        //Stop Loader, Stop Quote
+        quoteText.innerText = data.quoteText;
         complete();
     } catch (error) {
-        console.log ('OOOPS');
         getQuote();
     }
 }
 
-//Tweet Quote
 function tweetQuote() {
     const quote = quoteText.innerText;
     const author = authorText.innerText;
@@ -56,9 +52,7 @@ function tweetQuote() {
     window.open(twitterUrl, '_blank');
 }
 
-//Event Listeners
-newQuoteBtn.addEventListener('click', getQuote);
-twitterBtn.addEventListener('click', tweetQuote);
+newQuoteButton.addEventListener('click', getQuote);
+twitterButton.addEventListener('click', tweetQuote);
 
-//On Load Tasks
 getQuote();
